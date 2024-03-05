1ad8dfe53c61f487ed0f34a9e0b39baf5a3df4d3
AdamDjiddi <adoumdjiddi002@gmail.com>
2024-03-05 23:57:01 +0100

    Le code dans fichier road ameliore encore les bordures de la route

--- road.js
+++ road.js
@@ -1,5 +1,5 @@
 class Road{
-    constructor(x,width,lanecount=3){
+    constructor(x,width,lanecount=4){
         this.x=x;
         this.width;
         this.lanecount=lanecount;
@@ -11,6 +11,15 @@ class Road{
         this.top=-infinity;
         this.bottom=infinity;
 
+        const topLeft={x:this.left,y:this.top};
+        const topRight={x:this.right,y:this.top};
+        const bottomLeft={x:this.left,y:this.bottom};
+        const bottomRight={x:this.right,y:this.bottom};
+        this.borders=[
+            [topLeft,bottomLeft],
+            [topRight,bottomRight]
+        ];
+
     }
     getLaneCenter(laneIndex){
         const lanewidth=this.width/this.lanecount;
@@ -21,17 +30,14 @@ class Road{
         ctx.linewidth=5;
         ctx.strokeStyle="white";
 
-        for(let i=0;i<=this.lanecount;i++){
+        for(let i=1;i<=this.lanecount-1;i++){
             const x=lerp(
                 this.left,
                 this.right,
                 i/this.lanecount
             );
-            if(i>0 && i<this.lanecount){
-                ctx.setLineDash([20,20]);
-            }else{
-                ctx.setLineDash([]);
-            }
+            
+            ctx.setLineDash([20,20]);
             ctx.beginPath();
             ctx.moveTo(x,this.top);
             ctx.lineTo(x,this.bottom);
@@ -39,6 +45,14 @@ class Road{
 
         
         }
+        ctx.setLineDash([]);
+        this.borders.forEach(border=>{
+            ctx.beginPath();
+            ctx.moveTo(border[0].x,border[0].y);
+            ctx.lineTo(border[1].x,border[1].y);
+            ctx.stroke();
+
+        });
     }       
 }   
 
