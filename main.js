c6f31180f363443c7fa6bc72a109d36069404c78
AdamDjiddi <adoumdjiddi002@gmail.com>
2024-03-09 23:57:50 +0100

    On viens de créer un nouveau fichier nommé network.js pour la gestion de capteurs de la voiture en cas des obstacles

--- main.js
+++ main.js
@@ -3,9 +3,9 @@ canvas.width=200;
 
 const ctx = canvas.getContext("2d");
 const road=new Road(canvas.width/2,canvas.width*0.9);
-const car=new Car(road.getLaneCenter(1),100,30,50);
+const car=new Car(road.getLaneCenter(1),100,30,50,"KEYS");
 const traffic=[
-    new Car(road.getLaneCenter(1),-100,30,50)
+    new Car(road.getLaneCenter(1),-100,30,50,"DUMMY",2)
 ];
 
 console.log(road.getLaneCenter(1))
@@ -14,10 +14,10 @@ animate();
 
 function animate(){
     for(let i=0;i<traffic.length;i++){
-        traffic[i].update(road.borders);
+        traffic[i].update(road.borders,[]);
 
     }
-    car.update(road.borders);
+    car.update(road.borders,traffic);
 
     canvas.height=window.innerHeight;
 
@@ -25,7 +25,10 @@ function animate(){
     ctx.translate(0,-car.y+canvas.height*0.7);
 
     road.draw(ctx);
-    car.draw(ctx);
+    for(let i=0;i<traffic.length;i++){
+        traffic[i].draw(ctx,"red");
+    }
+    car.draw(ctx,"blue");
 
     ctx.restore();
     requestAnimationFrame(animate);
